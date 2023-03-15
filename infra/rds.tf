8a87ce6a4f94a3bc87207ef26a332ab74b5f557f
Matsumu Monday <mmatsumu19@gmail.com>
2023-03-15 07:40:41 -0700

    .

--- infra/rds.tf
+++ infra/rds.tf
@@ -1,19 +1,19 @@
-# resource "aws_db_instance" "main" {
-#   allocated_storage    = 10
-#   storage_type         = "gp2"
-#   engine               = "mysql"
-#   engine_version       = "5.7.16"
-#   instance_class       = "db.t2.micro"
-#   name                 = var.database_name
-#   username             = var.database_username
-#   password             = random_password.password.result
-#   db_subnet_group_name = "my_database_subnet_group"
-#   parameter_group_name = "default.mysql5.7"
-# }
+resource "aws_db_instance" "main" {
+  allocated_storage    = 10
+  storage_type         = "gp2"
+  engine               = "mysql"
+  engine_version       = "5.7.16"
+  instance_class       = "db.t2.micro"
+  name                 = var.database_name
+  username             = var.database_username
+  password             = random_password.password.result
+  db_subnet_group_name = "my_database_subnet_group"
+  parameter_group_name = "default.mysql5.7"
+}
 
 
-# resource "random_password" "password" {
-#   length           = 16
-#   special          = true
-#   override_special = "!#$%&*()-_=+[]{}<>:?"
-# }
+resource "random_password" "password" {
+  length           = 16
+  special          = true
+  override_special = "!#$%&*()-_=+[]{}<>:?"
+}
